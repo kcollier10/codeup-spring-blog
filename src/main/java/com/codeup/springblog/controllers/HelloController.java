564a329f1090a830888cf6b01b4b5d6b1e24978e
kcollier10 <kristencollier10@gmail.com>
2021-02-09 16:06:52 -0600

    feat: template files and HelloController methods

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,35 +1,47 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 // 1. import @Controller
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello from Spring!";
-    }
+//    @GetMapping("/hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello from Spring!";
+//    }
 
-    @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String hello(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String hello(@PathVariable String name) {
+//        return "Hello, " + name + "!";
+//    }
 
     // RequestingMapping can be a GET or a POST
     // Similar to CounterServlet from adlister repo
-    @RequestMapping(path ="/increment/{number}", method = RequestMethod.GET)
-    @ResponseBody
-    public String addOne(@PathVariable int number){
-        return number + " plus one is " + (number + 1) + "!";
-    }
+//    @RequestMapping(path ="/increment/{number}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String addOne(@PathVariable int number){
+//        return number + " plus one is " + (number + 1) + "!";
+//    }
 
     @GetMapping("/hello/{name}")
     public String sayHello(@PathVariable String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
     }
 }
