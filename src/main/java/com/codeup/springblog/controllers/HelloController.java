497d90e691e5b197b7b8863dfa018167319fb36c
kcollier10 <kristencollier10@gmail.com>
2021-02-10 10:14:29 -0600

    feat: spring view notes and lecture examples

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -30,8 +30,11 @@ public class HelloController {
 
     // {name} is our dynamic portion
     @GetMapping("/hello/{name}")
+    // Model class is part of Spring, not representing a model of our database!
     public String sayHello(@PathVariable String name, Model model) {
+        // "name" is our key to access info from front-end dynamically and name is our value/variable
         model.addAttribute("name", name);
+        // returning our hello.html
         return "hello";
     }
 
@@ -41,6 +44,8 @@ public class HelloController {
     }
 
     @PostMapping("/join")
+    // allowing us to capture something from the POST request (aka from our form)
+    // @RequestParam links to the item with form field 'cohort' in html
     public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model) {
         model.addAttribute("cohort", "Welcome to " + cohort + "!");
         return "join";
