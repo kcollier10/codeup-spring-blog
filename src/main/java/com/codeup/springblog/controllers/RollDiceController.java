e6f5502f43dfacd828582e52c70c7b8de4f60f75
kcollier10 <kristencollier10@gmail.com>
2021-02-10 11:26:20 -0600

    feat: allow user to see entry on redirected page

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -1,14 +1,22 @@
 package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RollDiceController {
 
-    @GetMapping("/roll-dice/")
-    public String roll() {
-        return "rollDice";
+    @GetMapping("/roll-dice")
+    public String roll(Model model) {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String numberPicked(@PathVariable int n, Model model) {
+        model.addAttribute("n", n);
+        return "rolled-dice";
     }
 
 }
