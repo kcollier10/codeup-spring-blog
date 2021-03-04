3516a1866743fc18a60491338cd622eabe52cb85
kcollier10 <kristencollier10@gmail.com>
2021-03-04 11:51:53 -0600

    feat: very rough draft of hamburger menu for responsive navbar

---
+++ src/main/resources/static/js/js.js
@@ -0,0 +1,9 @@
+"use strict";
+
+    let hamburger = document.getElementById('hamburgerbtn');
+
+    let mobileMenu = document.getElementById('mobileMenu');
+
+    hamburger.addEventListener('click', function(){
+    mobileMenu.classList.toggle('active');
+});
