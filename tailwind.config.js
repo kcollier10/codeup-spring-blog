7f044374d692522ea81af4042a68573d8bfa26c2
kcollier10 <kristencollier10@gmail.com>
2021-03-04 16:21:53 -0600

    feat: fixing links and adding custom colors to .json file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      greenLime: '#64FF5C',
+    },
     extend: {
       backgroundImage: theme => ({
         ... theme('user-images'),
