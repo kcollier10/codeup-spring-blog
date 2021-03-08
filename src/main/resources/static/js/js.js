802afe395ad702c609a83f545ac66ab0eacbc903
kcollier10 <kristencollier10@gmail.com>
2021-03-08 16:45:23 -0600

    feat: removing quotes api

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -13,10 +13,9 @@ function randomQuote() {
         dataType: "jsonp",
         data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
         success: function( response ) {
-            $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
-                response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
-            $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
-                ' (' + response.quoteAuthor + ')');
+
+            $("#random_quote").append("<p id='random_quote' class='lead text-center'>" +
+                response.quoteAuthor + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
         }
     });
 }
