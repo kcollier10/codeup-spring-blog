9dae95b2cd865fb1e2a2d59cbee9ce474752df46
kcollier10 <kristencollier10@gmail.com>
2021-03-09 11:22:45 -0600

    feat: testing API

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -7,23 +7,38 @@
 //     hamburger.addEventListener('click', function(){
 //     mobileMenu.classList.toggle('active');
 // });
-function randomQuote() {
-    $.ajax({
-        url: "https://api.forismatic.com/api/1.0/?",
-        dataType: "jsonp",
-        data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-        success: function( response ) {
+$(document).ready(function(){
 
-            $("#random_quote").append("<p id='random_quote' class='lead text-center'>" +
-                response.quoteAuthor + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+    $("#myform").submit(function(){
+
+        var search = $("#books").val();
+        if(search == "")
+        {
+            alert("Please enter something in the field");
         }
+        else{
+            var url = "";
+            var img = "";
+            var title = "";
+            var author = "";
+
+            $.get("https://www.googleapis.com/books/v1/volumes?q=" + search,function(response){
+
+                for(let i=0;i<response.items.length;i++)
+                {
+                    title=$('<h5 class="center-align white-text">' + response.items[i].volumeInfo.title + '</h5>');
+                    author=$('<h5 class="center-align white-text"> By:' + response.items[i].volumeInfo.authors + '</h5>');
+                    img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">Read More</button></a>');
+                    url= response.items[i].volumeInfo.imageLinks.thumbnail;
+                    img.attr('src', url);
+                    title.appendTo('#result');
+                    author.appendTo('#result');
+                    img.appendTo('#result');
+                }
+            });
+
+        }
+        return false;
     });
-}
 
-$(function() {
-    randomQuote();
-});
-
-$("button").click(function(){
-    randomQuote();
 });
