260cd2f7e5579bce79f13627f8e57c4305f16de9
kcollier10 <kristencollier10@gmail.com>
2021-03-09 11:49:14 -0600

    feat: adding scripts to templates

--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     $("#myform").submit(function(){
 
         let search = $("#books").val();
-        if(search == "")
+        if(search === "")
         {
             alert("Please enter something in the field");
         }
@@ -28,7 +28,7 @@ $(document).ready(function(){
                 {
                     title=$('<h5 class="center-align white-text">' + response.items[i].volumeInfo.title + '</h5>');
                     author=$('<h5 class="center-align white-text"> By:' + response.items[i].volumeInfo.authors + '</h5>');
-                    img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">Read More</button></a>');
+                    img = $('<img class="aligning card z-depth-5" id="dynamic" alt="image"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">Read More</button></a>');
                     url= response.items[i].volumeInfo.imageLinks.thumbnail;
                     img.attr('src', url);
                     title.appendTo('#result');
